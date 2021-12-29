61bff68edcce1d0a9c2db51e96aa6d211e0df73e
Khalin94 <mpzx1102@gmail.com>
2021-12-30 01:13:53 +0900

    git 연결 확인

---
+++ src/com/khalin/array/TempCap.java
@@ -0,0 +1,13 @@
+package com.khalin.array;
+
+public class TempCap {
+    public static void main(String[] args) {
+
+    }
+
+    private static int solution(int n, int[] student){
+
+
+        return 0;
+    }
+}
